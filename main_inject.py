f3df6d1e9e2951b5cccade919f320bb7989bd8aa
awadivkar <aavik.wadivkar@gmail.com>
2025-04-18 22:53:09 -0500

    changed main_inject to simulate radius power law, only test relevant inclinations, increase resolution of parameter space

--- main_inject.py
+++ main_inject.py
@@ -1,21 +1,20 @@
 import random
 import pandas as pd
-import csv
+import numpy as np
 from tqdm import tqdm
-import os
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 from preprocess import preprocess
-from injections import inject_transit
+from injections import inject_transit, calc_a
 from BLSFit import BLSfit, BLSResults, FoldedLC, BLSTestOutputs
-from BLStests import test_period, test_duration, test_depth, test_v_shape, test_snr, test_out_of_transit_variability, create_transit_mask_manual
+from BLStests import test_depth, test_v_shape, test_snr, test_out_of_transit_variability, create_transit_mask_manual
 
 df = pd.read_csv('tess_targets_data.csv')
-output_file = '/Users/aavikwadivkar/Documents/Exoplanets/Ampersand/albus/albus/data_outputs/injected_transits_output1.csv'
+output_file = '/Users/aavikwadivkar/Documents/Exoplanets/Ampersand/albus/albus/data_outputs/injected_transits_output2.csv'
 
 out = pd.read_csv(output_file)
 lc = None
 
-res = 5
+res = 10
 
 for i in range(res):
     for j in range(res):
@@ -23,7 +22,7 @@ for i in range(res):
             ID = str(i) + str(j) + str(k)
             print(ID)
             if int(ID) not in out['ID'].values:  #  Ensure check is against column values
-                lc = None  # Reset lc to None at the start of the iteration
+                lc = None  
                 
                 # Find random WD lightcurve
                 while lc is None:
@@ -33,21 +32,28 @@ for i in range(res):
                     try: lc = preprocess(tic_id, TICID=True)
                     except: pass
                 
+                r_p = ((i+1)/res)**2                        # Range of planet radii from 0.01 to 1
+                period = 1+(10/res)*j                       # Range of periods from 1 to 10 days
+                a = calc_a(0.6, period)/6.957*10**8         # Semi-major axis in meters
+                i_min = np.arccos((0.01+r_p)/a)/np.pi*180   # Minimum transit inclination in degrees
+                i = 90-(k*(90-i_min)/res)                   # Inclination from 90 to i_min degrees
+
                 # Inject transit
                 inj = inject_transit(ID, tic_id, lc, lc['time'].value,
                                 radius_star = 0.01, 
                                 mass_star = 0.6, 
-                                radius_planet = 1-(1/res)*i, 
+                                radius_planet = r_p * 0.01, 
                                 luminosity_star=0.001,
                                 albedo_planet=0.1, 
-                                period=1+(10/res)*j, 
-                                inclination=90-(1/res)*k)
+                                period=period,
+                                inclination=i
+                                )
                 
                 # Run BLS
                 results = BLSfit(inj)
-                high_periods, high_powers, best_period, t0, duration = BLSResults(results, folder='WD_Plots6', ID=ID)
+                high_periods, high_powers, best_period, t0, duration = BLSResults(results, folder='WD_Plots7', ID=ID)
                 for period in high_periods: 
-                    FoldedLC(inj, period, t0, ID=ID, folder='WD_Plots6', bin=False)                
+                    FoldedLC(inj, period, t0, ID=ID, folder='WD_Plots7', bin=False)                
                     # Run tests
                     depth = test_depth(inj['time'],
                                     inj['flux'],
